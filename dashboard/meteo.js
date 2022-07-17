18d0f52b8a8f9ee0675ff46e213d7e49a996c278
raphbag <raphbag772@gmail.com>
2022-07-17 16:32:40 +0200

    dashboard update

--- dashboard/meteo.js
+++ dashboard/meteo.js
@@ -1,11 +1,11 @@
 function buttonClickGET() {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=Le Pouliguen,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric"
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=Le Pouliguen,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric&lang=fr"
 
     $.get(url, callBackGetSuccess).done(function() {
         //alert( "second success" );
       })
       .fail(function() {
-        alert( "error" );
+        alert( "erreur" );
       })
       .always(function() {
         //alert( "finished" );
@@ -14,8 +14,12 @@ function buttonClickGET() {
 
 var callBackGetSuccess = function(data) {
     var element = document.getElementById("zone_meteo");
-    element.innerHTML = "La temperature de " + data.name + " est de " + data.main.temp + "°C";
+    element.innerHTML = data.main.temp + "°C";
     var imgelement = document.getElementById("img_meteo");
-    imgelement.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+    imgelement.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+    var element = document.getElementById("vitesse_vent");
+    element.innerHTML = data.wind.speed + " km/h";
+    var element = document.getElementById("city");
+    element.innerHTML = data.name;
 }
 
