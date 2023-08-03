425afa53032c7e17b87415e170631884c64eb575
raphbag <raphbag772@gmail.com>
2023-08-03 13:04:45 +0200

    update meteo API

--- assets/js/meteo.js
+++ assets/js/meteo.js
@@ -1,7 +1,7 @@
 function buttonClickGET() {
     var url = "https://api.openweathermap.org/data/2.5/weather?q=Le%20Pouliguen,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric&lang=fr"
 
-    $.get(url, callBackGetSuccess).done(function() {
+    $.get(url, callBack).done(function() {
         //alert( "second success" );
       })
       .fail(function() {
@@ -12,14 +12,9 @@ function buttonClickGET() {
       });
 }
 
-var callBackGetSuccess = function(data) {
-    var element = document.getElementById("zone_meteo");
-    element.innerHTML = data.main.temp + "°C";
+var callBack = function(data) {
     var imgelement = document.getElementById("img_meteo");
     imgelement.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-    var element = document.getElementById("vitesse_vent");
-    element.innerHTML = Math.round((data.wind.speed * 3.6) * 100) / 100 + " km/h";
-    // element.innerHTML = data.wind.speed * 3.6 + " km/h"
     var element = document.getElementById("city");
     element.innerHTML = data.name;
 }
