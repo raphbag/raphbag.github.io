493ddf8e5ede5c2a7de3cc3637f5fce942c6dffd
Raphaël <raphbag772@gmail.com>
2024-04-05 23:19:29 +0200

    add header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+	// JavaScript peut être ajouté ici pour le comportement du composant
+	let name = 'Mon Composant';
+</script>
+
+<h1>Hello {name}!</h1>
+
+<style>
+	/* Styles spécifiques au composant peuvent être ajoutés ici */
+	h1 {
+		color: blue;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import Header from '$components/Header.svelte';
+</script>
+
+<Header />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
