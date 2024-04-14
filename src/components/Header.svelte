88828957118f7ff11e428c51f6314c446840aa0c
Raphaël <raphbag772@gmail.com>
2024-04-14 17:26:46 +0200

    update header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,6 @@
 <script>
 	// JavaScript peut être ajouté ici pour le comportement du composant
-	let name = 'Mon Composant';
+	// let name = 'Mon Composant';
 
 	let links = [
 		{ href: '/', text: 'Home' },
@@ -9,10 +9,10 @@
 </script>
 
 <header>
-	<div>
+	<!-- <div>
 		Hello {name}!
-	</div>
-
+	</div> -->
+	<img src="../../logo.png" alt="Logo R du site" />
 	<nav>
 		{#each links as link}
 			<a href={link.href}>{link.text}</a>
@@ -22,7 +22,34 @@
 
 <style>
 	/* Styles spécifiques au composant peuvent être ajoutés ici */
+	header {
+		display: flex;
+		align-items: center;
+		padding: 10px 0px;
+		background-color: #f0f0f0;
+	}
+
+	nav {
+		padding: 20px;
+	}
+
 	nav > a {
 		margin-right: 10px;
+		color: black;
+		text-decoration: none;
+	}
+
+	nav > a:hover {
+		color: red;
+		transition: color 0.5s;
+	}
+	img {
+		width: 60px;
+		padding: 10px 20px;
+		border-right: 2px dotted gray;
+	}
+
+	img:hover {
+		cursor: pointer;
 	}
 </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,3 @@
-<h1>Mes projets</h1>
+<main>
+	<h1>Mes projets</h1>
+</main>
