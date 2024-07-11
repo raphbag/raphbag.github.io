5342ccb0336ca0575f13c24e7af7356698e573bc
Raphaël Baguelin <raphbag772@gmail.com>
2024-07-11 18:43:22 +0200

    home update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,33 @@
 <main>
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<h1>Bienvenue sur mon site</h1>
+	<div id="box1">
+		<p>Je suis Raphaël, un jeune étudiant en cybersécurité</p>
+		<img
+			src="https://cdn-icons-png.flaticon.com/512/11651/11651032.png"
+			alt="Menaces De Cybersécurité"
+		/>
+	</div>
+	<p><i>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</i></p>
 </main>
+
+<style>
+	main {
+		text-align: center;
+	}
+
+	#box1 {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	p {
+		padding: 0px 20px;
+	}
+
+	img {
+		border-left: 2px ridge gray;
+		width: 50px;
+		padding: 0px 20px;
+	}
+</style>
