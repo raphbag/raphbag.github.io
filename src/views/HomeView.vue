ea27d77c8c4fd8c504a3233aaba56f997173439a
Raphael <raphbag772@gmail.com>
2026-02-03 16:20:21 +0100

    feat: add some competences

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,158 @@
-<script setup></script>
+<script setup>
+const competences = [
+  {
+    name: 'HTML5',
+    type: 'language',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/1024px-HTML5_Badge.svg.png?20110131171049',
+    link: 'https://developer.mozilla.org/fr/docs/Web/HTML',
+  },
+  {
+    name: 'CSS3',
+    type: 'language',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/1920px-CSS3_logo.svg.png',
+    link: 'https://developer.mozilla.org/fr/docs/Web/CSS',
+  },
+  {
+    name: 'Python',
+    type: 'language',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/archive/c/c3/20220821155028%21Python-logo-notext.svg',
+    link: 'https://www.python.org/',
+  },
+  {
+    name: 'Cisco Packet Tracer',
+    type: 'tool',
+    img: 'https://cryptrz.org/wordpress/wp-content/uploads/2025/09/image.png',
+    link: 'https://www.netacad.com/cisco-packet-tracer',
+  },
+  {
+    name: 'GNS3',
+    type: 'tool',
+    img: 'https://avatars.githubusercontent.com/u/2739187?s=280&v=4',
+    link: 'https://www.gns3.com/',
+  },
+  {
+    name: 'Oracle VirtualBox',
+    type: 'tool',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/VirtualBox_2024_Logo.svg/langfr-1920px-VirtualBox_2024_Logo.svg.png',
+    link: 'https://www.virtualbox.org/',
+  },
+  {
+    name: 'Wireshark',
+    type: 'tool',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Wireshark_icon_new.png/960px-Wireshark_icon_new.png',
+    link: 'https://www.wireshark.org/',
+  },
+  {
+    name: 'AWS',
+    type: 'cloud',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Amazon_Web_Services_2025.svg/960px-Amazon_Web_Services_2025.svg.png',
+    link: 'https://aws.amazon.com/fr/',
+  },
+  {
+    name: 'Google Cloud',
+    type: 'cloud',
+    img: 'https://play-lh.googleusercontent.com/RyoQTmHnxsxPYabsETmWVXHtLorVh_yOO48hsdv2VmI-Uki4qt5c5vV1cicJODV56A4',
+    link: 'https://cloud.google.com/',
+  },
+  {
+    name: 'Debian',
+    type: 'os',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Openlogo-debianV2.svg/500px-Openlogo-debianV2.svg.png',
+    link: 'https://www.debian.org/',
+  },
+  {
+    name: 'Kali Linux',
+    type: 'os',
+    img: 'https://www.kali.org/docs/policy/trademark/kali-dragon-tm.png',
+    link: 'https://www.kali.org/',
+  },
+  {
+    name: 'Windows',
+    type: 'os',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Windows_logo_-_2021.svg/960px-Windows_logo_-_2021.svg.png',
+    link: 'https://www.microsoft.com/fr-fr/windows',
+  },
+]
+</script>
 
 <template>
   <main>
     <section class="hero">
       <h1 class="hero__title">Hi, I'm Raphaël</h1>
-      <h2 class="hero__subtitle">Je suis passionné par l'informatique et les nouvelles technologies.<br/> Je suis actuellement étudiant en cybersécurité.</h2>
+      <h2 class="hero__subtitle">
+        Je suis passionné par l'informatique et les nouvelles technologies.<br />
+        Je suis actuellement étudiant en cybersécurité.
+      </h2>
     </section>
     <section class="competences">
-      <h2>Mes compétences</h2>
+      <h1>Mes compétences</h1>
+      <h2>Langages</h2>
       <div class="container">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/1024px-HTML5_Badge.svg.png?20110131171049"
-          alt="Logo HTML5"
-          title="HTML5"
-        />
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/1920px-CSS3_logo.svg.png"
-          alt="Logo CSS3"
-          title="CSS3"
-        />
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/archive/c/c3/20220821155028%21Python-logo-notext.svg"
-          alt="Logo Python"
-          title="Python"
-        />
-        <img
-          src="https://avatars.githubusercontent.com/u/2739187?s=280&v=4"
-          alt="Logo Logiciel GNS3"
-          title="GNS3"
-        />
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/VirtualBox_2024_Logo.svg/langfr-1920px-VirtualBox_2024_Logo.svg.png"
-          alt="Logo de Oracle VirtualBox"
-          title="Oracle VirtualBox"
-        />
+        <a
+          v-for="competence in competences.filter((c) => c.type === 'language')"
+          :key="competence.name"
+          :href="competence.link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            :key="competence.name"
+            :src="competence.img"
+            :alt="`Logo ${competence.name}`"
+            :title="competence.name"
+          />
+        </a>
+      </div>
+      <h2>Outils</h2>
+      <div class="container">
+        <a
+          v-for="competence in competences.filter((c) => c.type === 'tool')"
+          :key="competence.name"
+          :href="competence.link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            :key="competence.name"
+            :src="competence.img"
+            :alt="`Logo ${competence.name}`"
+            :title="competence.name"
+          />
+        </a>
+      </div>
+      <h2>Cloud</h2>
+      <div class="container">
+        <a
+          v-for="competence in competences.filter((c) => c.type === 'cloud')"
+          :key="competence.name"
+          :href="competence.link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            :key="competence.name"
+            :src="competence.img"
+            :alt="`Logo ${competence.name}`"
+            :title="competence.name"
+          />
+        </a>
+      </div>
+      <h2>Systèmes d'exploitation</h2>
+      <div class="container">
+        <a
+          v-for="competence in competences.filter((c) => c.type === 'os')"
+          :key="competence.name"
+          :href="competence.link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            :key="competence.name"
+            :src="competence.img"
+            :alt="`Logo ${competence.name}`"
+            :title="competence.name"
+          />
+        </a>
       </div>
     </section>
   </main>
@@ -46,9 +165,7 @@
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 1.5rem;
   gap: 1rem;
-  background: linear-gradient(145deg, var(--vt-c-black), #161616);
 }
 
 .hero__title {
@@ -80,7 +197,6 @@
   justify-content: center;
   padding: 1.5rem;
   gap: 1rem;
-  background: linear-gradient(145deg, var(--vt-c-black), #161616);
 }
 
 .container {
@@ -88,7 +204,8 @@
   align-items: center;
   justify-content: center;
   flex-wrap: wrap;
-  padding: 1.5rem;
+  padding: 1.5rem 0;
+  margin-bottom: 1rem;
   gap: 1rem;
 }
 
@@ -96,8 +213,9 @@
   width: 100px;
   height: 100px;
   border-radius: 10px;
+  object-fit: contain;
   padding: 10px;
-  background-color: #242424;
+  background-color: #333333;
   box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
   transition: all 0.3s ease;
 }
